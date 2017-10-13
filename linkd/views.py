37a3b81cc48e9946c73d23323968269557f9d188
Rahul <rs280599@gmail.com>
2017-10-13 16:03:16 +0530

    minor mistake

--- linkd/views.py
+++ linkd/views.py
@@ -31,17 +31,3 @@ def getmentor(request,mentor_id):
 			'mentor':mentor,
 			'dictionary':dictionary,
 		})
-
-def favorite(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song= album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        context = {'album' : album,
-            'error_message' : "You didnt selected a valid song"}
-        return render(request,'music/detail.html',context)
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        context = {'album' : album}
-        return render(request,'music/detail.html',context)
