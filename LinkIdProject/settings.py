3e4d20e04013ce35afb8f102e9aa18c909278d14
vinay <vinay@vinay.com>
2017-12-06 13:12:25 +0530

    python manage.py collectstatic --noinput

--- LinkIdProject/settings.py
+++ LinkIdProject/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/linkd/static')
-print(STATIC_ROOT)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'linkd/static'),
 )
